4aad41dc4b59a06a3dc216f723598c16278ae6c3
Andres Vargas <andrevargas@coca-cola.com>
2020-04-10 23:38:04 -0500

    update data

--- demo/static/src/Shared/MainMenu.vue
+++ demo/static/src/Shared/MainMenu.vue
@@ -2,9 +2,7 @@
   <div>
     <div class="mb-4">
       <inertia-link class="flex items-center group py-3" :href="route('demo:dashboard')">
-        
-        <icon name="dashboard" class="w-4 h-4 mr-2" :class="isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'">
-          </icon>
+        <icon name="dashboard" class="w-4 h-4 mr-2" :class="isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'"></icon>
         <div :class="isUrl('') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Dashboard</div>
       </inertia-link>
     </div>
