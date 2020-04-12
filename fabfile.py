2d4139dc84189a1300816858f85620ecd6462d62
Andres Vargas <andrevargas@coca-cola.com>
2020-04-11 19:00:05 -0500

    update data

--- fabfile.py
+++ fabfile.py
@@ -3,10 +3,14 @@ from fabric import task
 
 @task
 def deploy(ctx):
-    with ctx.prefix("source ~/.virtualenvs/inertia/bin/bash") and \
-            ctx.cd("apps/django-inertia-demo"):
-        ctx.run("pip install -r requirements.txt")
-        ctx.run("git pull")
+    with ctx.prefix("source ~/.virtualenvs/inertia/bin/activate"):
+        with ctx.cd("apps/django-inertia-demo"):
+            ctx.run("git pull")
+            ctx.run("pip install -r requirements.txt")
+            #ctx.run("npm i")
+            ctx.run("npm run build")
+            ctx.run("python manage.py collectstatic --noinput")
+            ctx.sudo("supercvisorctl restart inertia")
 
 
 
