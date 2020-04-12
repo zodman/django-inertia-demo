980f8699b51b3797510f7559d2b9060d506fbdad
Andres Vargas <andrevargas@coca-cola.com>
2020-04-11 19:01:29 -0500

    update settings

--- app/settings.py
+++ app/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 JS_ROUTES_INCLUSION_LIST = [
--- fabfile.py
+++ fabfile.py
@@ -7,8 +7,6 @@ def deploy(ctx):
         with ctx.cd("apps/django-inertia-demo"):
             ctx.run("git pull")
             ctx.run("pip install -r requirements.txt")
-            #ctx.run("npm i")
-            ctx.run("npm run build")
             ctx.run("python manage.py collectstatic --noinput")
             ctx.sudo("supercvisorctl restart inertia")
 
