6cc42ed02365e9378938681ebe9490fca2b4b38f
Andres Vargas <andres@python3.ninja>
2020-09-09 09:19:29 -0500

    update changes

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,15 @@
 from fabric import task
-
+from invoke import run
+from patchwork.transfers import rsync
 
 @task
 def deploy(ctx):
+    run("yarn install", echo=True)
+    run("yarn run build", echo=True)
+    run("python manage.py collectstatic --noinput", echo=True)
+    run("find . -name '__pycache__' |xargs rm -rf ", echo=True)
+    rsync(ctx, "static/", "apps/django-inertia-demo/")
+
     with ctx.prefix("source ~/.virtualenvs/inertia/bin/activate"):
         with ctx.cd("apps/django-inertia-demo"):
             ctx.run("git pull")
@@ -11,6 +18,4 @@ def deploy(ctx):
             ctx.run("python populate.py")
             ctx.run("python manage.py collectstatic --noinput")
     ctx.run("sudo supervisorctl restart inertia")
-
-
 
